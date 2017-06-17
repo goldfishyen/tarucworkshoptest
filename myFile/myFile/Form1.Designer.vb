fd9b09ee55bb4592dae4a6661f30f725da1020f0
User <User@B165>
2017-06-17 11:15:32 +0800

    create new branch

--- myFile/myFile/Form1.Designer.vb
+++ myFile/myFile/Form1.Designer.vb
@@ -28,11 +28,11 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.Size = New System.Drawing.Size(96, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "create new branch"
         '
         'Form1
         '

be46b60773ddbdb6b87bcdccce8bd96ba3589bb0
User <User@B165>
2017-06-17 11:33:45 +0800

    Add New Friend Branch

--- myFile/myFile/Form1.Designer.vb
+++ myFile/myFile/Form1.Designer.vb
@@ -30,9 +30,9 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(13, 13)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.Size = New System.Drawing.Size(91, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "New firnd branch "
         '
         'Form1
         '
